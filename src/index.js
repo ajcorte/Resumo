db1639cd4dbc7ff22d9f5d1745e6d33270061e29
ajcorte <ajcorte@gmail.com>
2023-12-18 19:37:53 +0000

    commit1

---
+++ src/index.js
@@ -0,0 +1,37 @@
+//Declarando variaveis
+console.log("Digite o nome do jogador:");
+//declarar uma variavel
+let nickname = "ajcorte";
+//concatenando uma mensagem fixa + uma variavel
+console.log("Bem vindo " + nickname +"!");
+console.log("Atencao, jogador " + nickname + " entrou no servidor!");
+
+//Declarando constantes
+const notificacao = "Pokemon Go diz: ";
+//Saida
+console.log(notificacao + "Tem um novo pokemon no game!");
+console.log(notificacao + "Voce foi derrotado por um lider Ouro!");
+
+//Exemplo pratico
+//Variaveis
+let poteCafe = "Cafe";
+let poteAcucar = "Acucar";
+let poteBiscoito = "Biscoito";
+let mensagem = "Na cozina hoje tem: ";
+//Saida
+console.log(mensagem +
+poteCafe + " - " +
+poteAcucar + " - " +
+poteBiscoito);
+
+//Pokemon
+let nomePokemon = "Pikachu";
+let pokemonSexo = "M";
+let nivelPokemon = 20;
+let pontosDeVidaPokemon = 45;
+let selecionavel = false;
+
+console.log("O Pokemon " + nomePokemon +
+" eh do sexo " + pokemonSexo +
+", esta no nivel " + nivelPokemon +
+" e tem " + pontosDeVidaPokemon + " pontos de vida.");
