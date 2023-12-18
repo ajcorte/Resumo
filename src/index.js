857ed6f83077949717195bd7f4efbf78c01cfa57
ajcorte <ajcorte@gmail.com>
2023-12-18 21:28:26 +0000

    commit2

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ let nomesPokemon = ["Pikachu" , "Chamander" , "Bubassaur"];
 let timePokemon = [
   ["Pikachu" , "M" , 1],
   ["Chamander", "F" , 3]
-]
+];
 // Linhas e Colunas
 //      C[0]   C[1]   C[2]
 //L[0] [0][0] [0][1] [0][2]
@@ -51,3 +51,44 @@ console.log(
 "O Pokemon " + timePokemon[1][0]  + 
 " eh do sexo " + timePokemon[1][1] +
  " e esta no nivel " + timePokemon[1][2]);
+
+ //Operadores Aritmeticos
+ let adicao = 5 + 3;
+ console.log(adicao);
+
+ let subtracao = 8 - 2;
+ console.log(subtracao);
+
+ let multiplicacao = 4 * 6;
+ console.log(multiplicacao);
+
+ let divisao = 10 / 2;
+ console.log(divisao);
+
+ let modulo = 10 % 3;
+ console.log(modulo);
+
+ //Operadores Relacionais
+ let igual = (5 == 5);
+ console.log(igual);
+
+ let diferente = (5 != 6);
+ console.log(diferente);
+
+ let maiorQue = (5 > 4);
+ console.log(maiorQue);
+
+ let menorQue = (4 < 5);
+ console.log(menorQue);
+
+ let maiorIgual = (5 >= 4);
+ console.log(maiorIgual);
+
+ let menorIgual = (4 <= 5);
+ console.log(menorIgual);
+
+ //Operadores Logicos
+ //Operadores de Atribuicao
+ //Operadores de Incremento e Decremento
+
+ 
